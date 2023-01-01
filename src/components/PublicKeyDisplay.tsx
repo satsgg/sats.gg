5c2c27e13033f0f6542391739fd2efc4f4fc7ca4
Chad Welch <cwelc001@gmail.com>
2023-01-01 08:16:35 -0800

    remove unused components

--- src/components/PublicKeyDisplay.tsx
+++
@@ -1,8 +0,0 @@
-interface PublicKeyDisplayProps {
-  publicKey: string
-  length?: number
-}
-
-export const PublicKeyDisplay = ({ publicKey, length = 24 }: PublicKeyDisplayProps) => {
-  return <>{publicKey.slice(0, length)}...</>
-}
