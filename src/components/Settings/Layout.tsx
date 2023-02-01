155f9b34ee4ad8a3410ad90210fb8554e47ca828
Chad Welch <cwelc001@gmail.com>
2023-02-01 10:17:32 -0800

    add settings tabs

--- src/components/Settings/Layout.tsx
+++ src/components/Settings/Layout.tsx
@@ -1,23 +1,29 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 import { ReactNode, ReactElement } from 'react'
 import useAuthStore from '~/store/useAuthStore'
 import { DefaultLayout } from '../DefaultLayout'
 import { Spinner } from '../Spinner'
 
+const TabButton = ({ tab, current = false }: { tab: string; current: boolean }) => {
+  const selectedStyle = 'text-orange-400 border-b border-orange-400'
+  const styling = current ? selectedStyle : ''
+  return (
+    <Link href={'/settings/' + tab}>
+      <span
+        className={`${styling} py-1 capitalize hover:cursor-pointer hover:border-b hover:border-orange-400 hover:text-orange-400`}
+      >
+        {tab}
+      </span>
+    </Link>
+  )
+}
+
 const SettingsLayout = ({ children }: { children: ReactNode }) => {
   const { status: authStatus } = useAuthStore()
   const router = useRouter()
 
-  const highlightTab = () => {
-    // console.log(router.pathname) === '/settings/profile'
-    if (router.pathname.includes('profile')) {
-      return 'profile'
-    } else if (router.pathname.includes('relays')) {
-      return 'relays'
-    } else {
-      return 'unknown'
-    }
-  }
+  const tabs = ['profile', 'stream', 'relays']
 
   const getContent = () => {
     if (authStatus === 'authenticated') {
@@ -35,11 +41,14 @@ const SettingsLayout = ({ children }: { children: ReactNode }) => {
 
   return (
     <DefaultLayout>
-      <div className="w-full bg-stone-900 py-10 px-8 text-white">
-        {/* TODO: Make border under psuedo tabs */}
-        <h1 className="mb-6 border-b border-gray-500 pb-4 text-4xl font-bold">Settings</h1>
+      <div className="w-full space-y-4 bg-stone-900 py-8 px-8 text-white">
+        <h1 className="text-4xl font-bold ">Settings</h1>
 
-        <p>{highlightTab()}</p>
+        <div className="flex space-x-6 border-b border-gray-500">
+          {tabs.map((tab) => {
+            return <TabButton tab={tab} current={router.pathname.includes(tab)} />
+          })}
+        </div>
 
         {getContent()}
       </div>
