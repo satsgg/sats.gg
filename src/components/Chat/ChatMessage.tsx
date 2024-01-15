fffc9da3bf1c3bae26d725d9b909f4a500d19f7f
Chad Welch <cwelc001@gmail.com>
2024-01-15 18:02:16 -0500

    chat message use opacity-0 instead of hidden

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -7,7 +7,7 @@ const ChatMessage = ({ channelPubkey, note }: { channelPubkey: string; note: Nos
   const { profile, isLoading } = useProfile(note.pubkey)
 
   return (
-    <div className={`break-words px-3 ${isLoading ? 'hidden' : ''}`}>
+    <div className={`break-words px-3 ${isLoading ? 'opacity-0' : ''}`}>
       <ChatUser channelPubkey={channelPubkey} pubkey={note.pubkey} profile={profile} />
       <span className="text-sm text-white">: </span>
       <Message content={note.content} />
--- src/components/Chat/ZapChatMessage.tsx
+++ src/components/Chat/ZapChatMessage.tsx
@@ -18,7 +18,7 @@ const ZapChatMessage = ({
   const { profile, isLoading } = useProfile(pubkey)
 
   return (
-    <div className={`break-words px-2 py-1 ${isLoading ? 'hidden' : ''}`}>
+    <div className={`break-words px-2 py-1 ${isLoading ? 'opacity-0' : ''}`}>
       <div className="flex rounded bg-primary bg-opacity-10 px-1 py-1">
         <div className="text-sm text-white">
           <LightningBolt className="inline-flex" height={15} width={15} strokeWidth={1.5} />
