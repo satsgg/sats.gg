2d65de4bcfddfc460264c2e9367680076630ec44
Chad Welch <cwelc001@gmail.com>
2024-02-04 13:51:07 -0500

    forgot to set Button height back to 8

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,8 +9,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button: React.FC<ButtonProps> = ({ className, disabled = false, onClick, icon, children, ...rest }) => {
   const defaultStyle =
-    // 'relative inline-flex h-8 items-center justify-center gap-1 rounded bg-primary-500 px-3 py-2 text-center text-sm font-semibold capitalize shadow-md hover:bg-primary-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-lg disabled:cursor-not-allowed disabled:bg-stone-700'
-    'relative inline-flex items-center justify-center gap-1 rounded bg-primary-500 px-3 py-2 text-center text-sm font-semibold capitalize shadow-md hover:bg-primary-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-lg disabled:cursor-not-allowed disabled:bg-stone-700'
+    'relative inline-flex h-8 items-center justify-center gap-1 rounded bg-primary-500 px-3 py-2 text-center text-sm font-semibold capitalize shadow-md hover:bg-primary-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-lg disabled:cursor-not-allowed disabled:bg-stone-700'
   const styles = className ? `${className} ${defaultStyle}` : defaultStyle
   return (
     <button type="button" onClick={onClick} disabled={disabled} className={styles} {...rest}>
