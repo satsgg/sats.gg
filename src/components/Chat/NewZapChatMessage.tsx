52d26fa99098d3b4b30aa1688a973ab559574101
Chad Welch <cwelc001@gmail.com>
2025-02-11 22:10:38 -0500

    denser glow

--- src/components/Chat/NewZapChatMessage.tsx
+++ src/components/Chat/NewZapChatMessage.tsx
@@ -23,10 +23,10 @@ const ZapChatMessage = ({
 
   return (
     <div className="relative mx-2 my-2">
-      <div className="absolute -inset-[4px] rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 opacity-50 blur-[4px]"></div>
-      <div className="absolute -inset-[2px] rounded-lg bg-gradient-to-r from-purple-400 to-pink-400 opacity-50 blur-[1px]"></div>
-      <div className="absolute -inset-[2px] rounded-lg bg-gradient-to-r from-purple-400 to-pink-400 opacity-75"></div>
-      <div className="relative flex items-start rounded-lg bg-background/95 px-2 py-2 backdrop-blur-[2px]">
+      <div className="absolute -inset-[6px] rounded-xl bg-gradient-to-r from-purple-500 to-pink-500 opacity-40 blur-[6px]"></div>
+      <div className="absolute -inset-[3px] rounded-lg bg-gradient-to-r from-purple-400 to-pink-400 opacity-40 blur-[3px]"></div>
+      <div className="absolute -inset-[2px] rounded-lg bg-gradient-to-r from-purple-400 to-pink-400 opacity-100"></div>
+      <div className="relative flex items-start rounded-lg bg-background px-2 py-2">
         <Avatar className="mr-1 h-5 w-5 flex-shrink-0">
           <AvatarImage src={profile?.picture} alt={'Profile Picture'} />
           <AvatarFallback>{pubkey.slice(0, 2).toUpperCase()}</AvatarFallback>
