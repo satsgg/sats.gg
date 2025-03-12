ce0bff62fdb3f9347b6a6c68b4a0f1356331c2ac
Chad Welch <cwelc001@gmail.com>
2025-03-12 11:10:21 -0500

    fix landing page buttons, remove zap logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import { Zap, Twitter, Globe, Newspaper, Layout, Share2 } from 'lucide-react'
-import { buttonVariants } from '@/components/ui/button'
+import { Twitter, Globe, Newspaper, Layout, Share2 } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import { cn } from '@/lib/utils'
 
 export default function LandingPage() {
   const platforms = [
@@ -16,8 +15,8 @@ export default function LandingPage() {
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 text-foreground">
       <div className="mx-auto w-full max-w-md space-y-8 text-center md:max-w-4xl">
         <div className="flex flex-col items-center gap-4">
-          <Zap className="size-16 md:size-20 text-primary" />
-          <h1 className="text-5xl font-bold md:text-7xl">sats.gg</h1>
+          {/* <Zap className="size-16 md:size-20 text-primary" /> */}
+          <h1 className="text-5xl font-bold md:text-7xl">SATS.GG</h1>
         </div>
 
         <p className="whitespace-pre-line text-2xl font-medium text-muted-foreground md:text-4xl">
@@ -25,26 +24,22 @@ export default function LandingPage() {
         </p>
 
         <div className="flex flex-col justify-center gap-4 pt-8 sm:flex-row">
-          <Link
-            href="/stream"
-            className={cn(buttonVariants({ size: 'lg' }), 'flex-1 text-lg sm:min-w-[160px] sm:flex-initial')}
-          >
-            Go Live
+          <Link href="/stream">
+            <Button variant="default" size="lg">
+              Go Live
+            </Button>
           </Link>
 
-          <Link
-            href="/browse"
-            className={cn(
-              buttonVariants({ variant: 'outline', size: 'lg' }),
-              'flex-1 text-lg sm:min-w-[160px] sm:flex-initial',
-            )}
-          >
-            Watch
+          <Link href="/watch">
+            <Button variant="outline" size="lg">
+              Watch
+            </Button>
           </Link>
         </div>
 
         <div className="space-y-6 pt-16">
-          <p className="text-sm text-muted-foreground">Monetized content available on your favorite platforms:</p>
+          {/* <p className="text-sm text-muted-foreground">Monetized content available on your favorite platforms:</p> */}
+          <p className="text-sm text-muted-foreground">Monetize your content on your favorite platforms.</p>
           <div className="flex flex-wrap justify-center gap-8">
             {platforms.map((platform) => (
               <div
